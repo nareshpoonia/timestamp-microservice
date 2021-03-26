63172d6d23a848179a8448b2f77708847294c96c
Naresh Poonia <nareshpoonia7@gmail.com>
2021-03-27 02:07:33 +0530

    changed app.get url address

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-app.get("/api/timestamp/:date_string", function (req, res) {
+app.get("/api/timestamp", function (req, res) {
   var todaysDate = new Date()
   res.json({ "unix" : todaysDate.getTime(),
   "utc": todaysDate.toUTCString()  
