fb412c82d92c61b81e1fd62f415ef4f0b2894540
Naresh Poonia <nareshpoonia7@gmail.com>
2021-03-28 01:52:50 +0530

    Giving unix and utc key value pair if the date is valid

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
+  console.log({greeting: 'hello API'});
   res.json({greeting: 'hello API'});
+  
 });
 
 app.get("/api/timestamp", function (req, res) {
@@ -31,7 +33,17 @@ app.get("/api/timestamp", function (req, res) {
   });
 });
 app.get("/api/timestamp/:date_string", function (req, res) {
-  res.json({ error : "Invalid Date" });
+  let dateString= req.params.date_string;
+  let datePassed = new Date(dateString);
+  
+  if (datePassed=="Invalid Date"){
+  res.json({
+     error : "Invalid Date" })}
+     else {res.json({
+       "unix" : datePassed.getTime(),
+       "utc" : datePassed.toUTCString() 
+     })}
+     ;
 });
 
 
