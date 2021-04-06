d8912f51e308b1b75729862d4b9d0eeae4e41f90
Naresh Poonia <nareshpoonia7@gmail.com>
2021-04-06 20:22:47 +0530

    Add Mongoose, MongoDB, body-parser and short id to package.json Added necessary code to add short url Updated index.js for a seperate url for Url Shortner project

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
+var database_uri ='mongodb+srv://naresh-poonia-fcc-coding-challenges:break4200@naresh-poonia-fcc-codin.miflq.mongodb.net/naresh-poonia-fcc-coding-challenges?retryWrites=true&w=majority'
 // server.js
 // where your node app starts
 
 // init project
 var express = require('express');
 var app = express();
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var shortid = require('shortid');
+
+//mongoose.connect(process.env.DB_URI);
+mongoose.connect(
+database_uri, {
+  useNewUrlParser: true, 
+  useUnifiedTopology:true 
+});
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+var bodyParser = require('body-parser');
+const { response } = require('express');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -20,6 +36,10 @@ app.get("/", function (req, res) {
 
 app.get("/requestHeaderParser", function (req, res) {
   res.sendFile(__dirname + '/views/requestHeaderParser.html');
+});
+
+app.get("/urlShortner", function (req, res) {
+  res.sendFile(__dirname + '/views/urlShortner.html');
 });
 
 // your first API endpoint... 
@@ -35,6 +55,8 @@ app.get("/api/timestamp", function (req, res) {
   "utc": todaysDate.toUTCString()  
   });
 });
+
+//timestamp project
 app.get("/api/timestamp/:date_string", function (req, res) {
   let dateString= req.params.date_string;
   if (parseInt(dateString)>10000){
@@ -54,6 +76,8 @@ app.get("/api/timestamp/:date_string", function (req, res) {
      })}
      ;
 });
+
+//request header parser
 app.get("/api/whoami", function (req, res) {
   res.json({"ipaddress" : req.connection.remoteAddress,
     "language" : req.headers["accept-language"],
@@ -61,8 +85,69 @@ app.get("/api/whoami", function (req, res) {
   
 });
 
+//URL Shortner
+//Built a schema and model to store saved URLS
+var ShortURL = mongoose.model('ShortURL', new mongoose.Schema({
+short_url : String,
+original_url: String,
+suffix: String
+}));
 
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:false}))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.post("/api/shorturl/new", function (req, res) {
+  let client_requested_url =req.body.url
+  let suffix= shortid.generate()
+  let newShortURL= suffix
+  console.log(client_requested_url,"<= this will be our original url")
+  console.log(suffix,"<= this will be our suffix")
+
+  let newURL = new ShortURL({
+    short_url: __dirname + "/api/shorturl/"+suffix,
+    original_url: client_requested_url,
+    suffix: suffix
+  })
+
+  
+  newURL.save(function(err,doc){
+    if(err) return console.error(err);
+    res.json({
+      "saved": true,
+      "short-url" : newURL.short_url,
+      "original-url": newURL.original_url,
+      "suffix":newURL.suffix
+    });
+  });
+
+
+
+});
+app.get("/api/shorturl/:suffix", (req, res, next) => {
+  let userGeneratedSuffix = req.params.suffix;
+  ShortURL.find({suffix: userGeneratedSuffix}).then(foundUrls => {
+    let urlForRedirect = foundUrls[0];
+    console.log(urlForRedirect,"uslForRedirect")
+    console.log(foundUrls, "this is foundurl")
+    console.log(typeof(foundUrls), "this is our type of foundurl")
+    console.log(typeof(foundUrls[0]),"this is our type of foundurl[0]")
+    console.log(foundUrls[0],"this is foundURL[0]")
+    console.log(urlForRedirect.original_url)
+    if(urlForRedirect.original_url)
+      res.status(200).send('<script>window.location.href="http://'+urlForRedirect.original_url+'"</script>');
+
+    //res.status(200).send('<script>window.location.href="https://google.com"</script>');
+    //res.redirect(urlForRedirect.original_url)
+    next();
+  });
+  
+});
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
